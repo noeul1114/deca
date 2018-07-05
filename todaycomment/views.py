da0f35df30bd8f4910f90ea723450a10ee5072cc
willypower <parkhyongsok@naver.com>
2018-07-06 00:06:24 +0900

    todaycomment in prgress
    updating edit/delete button on comments

--- todaycomment/views.py
+++ todaycomment/views.py
@@ -13,17 +13,17 @@ def com_index(request):
     return render(request, 'todaycomment/index.html', {'comment_list': comment_list})
 
 def com_post(request):
-    C = Comment_list(comment_text=request.POST['comment_text'], created_at=timezone.now())
     try:
+        C = Comment_list(comment_text=request.POST['comment_text'], created_at=timezone.now())
         C.save()
         return HttpResponseRedirect(reverse('todaycomment:com_index'))
     except:
-        return HttpResponseRedirect('todaycomment:com_index')
+        return render(request, 'todaycomment/index.html', {'error_message': "댓글을 쓰는데에 실패했습니다."})
 
 def com_delete(request):
-    C = Comment_list.objects.get(id=request.POST['comment_del'])
+    C = get_object_or_404(Comment_list, id=request.POST['comment_del'])
     try:
         C.delete()
         return HttpResponseRedirect(reverse('todaycomment:com_index'))
     except:
-        return HttpResponseRedirect('todaycomment:com_index')
+        return render(request, 'todaycomment/index.html', {'error_message' : "댓글을 삭제하는데에 실패했습니다."})

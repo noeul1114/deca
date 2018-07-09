6c94c6ecd1465b61e7d9504fd9da1b0bf75494a0
Peria <parkhyongsok@naver.com>
2018-07-10 00:44:34 +0900

    boards app in progress
    board article admin register

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Comment, Article
+
 # Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['article_text']}),
+        ('Date information', {'fields': ['created_at'], 'classes': ['collapse']}),
+    ]
+    inlines = [CommentInline]
+    list_display = ('title', 'article_text', 'writer')
+    list_filter = ['created_at']
+    search_fields = ['writer']
+
+
+admin.site.register(Article, ArticleAdmin)

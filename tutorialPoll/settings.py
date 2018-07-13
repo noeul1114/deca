cfcbdae827815696d99e2477113300bcd47a2747
Peria <parkhyongsok@naver.com>
2018-07-13 14:42:11 +0900

    Static deployment settings

--- tutorialPoll/settings.py
+++ tutorialPoll/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# For deployment
+STATIC_URL = 'https://willypower.cafe24.com/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# For development
+#STATIC_URL = '/static/'
+#STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

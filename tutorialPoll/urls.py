92d918108e85e6b3c7c5bc8167aa48a7f2ebd805
Peria <parkhyongsok@naver.com>
2018-07-14 12:39:15 +0900

    boards app in progress

--- tutorialPoll/urls.py
+++ tutorialPoll/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('polls.urls')),
-    path('todaycomment/', include('todaycomment.urls')),
-    path('boards/', include('boards.urls')),
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('boards.urls')),
+    # path('todaycomment/', include('todaycomment.urls')),
+    # path('boards/', include('boards.urls')),
+    # path('polls/', include('polls.urls')),
+    path('say/admin/', admin.site.urls),
 ]

4ac55a405885fdf172e5477e7889ec50711caeb7
Peria <parkhyongsok@naver.com>
2018-07-25 00:02:21 +0900

    boards app in progress

--- tutorialPoll/settings.py
+++ tutorialPoll/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'tutorialPoll.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sayproject',
-        'USER': 'sayproject',
-        'PASSWORD': 'qudtlstz1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'sayproject',
+#         'USER': 'sayproject',
+#         'PASSWORD': 'qudtlstz1',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 

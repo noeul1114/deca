eac421976b8887a2d4a2601dea5b80b170172001
willypower <parkhyongsok@naver.com>
2018-08-01 00:09:21 +0900

    postgres edit

--- tutorialPoll/settings.py
+++ tutorialPoll/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'tutorialPoll.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'posttgresql',
+        'NAME': 'postgresql',
         'USER': 'django',
         'PASSWORD': 'qudtlstz1',
         'HOST': '127.0.0.1',

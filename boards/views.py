f949f80a354fef0303f871936b1d0a393a882d8c
willypower <parkhyongsok@naver.com>
2019-01-13 19:24:47 +0900

    IP get testing

--- boards/views.py
+++ boards/views.py
@@ -437,39 +437,43 @@ def board_comment_vote(request, comment_id):
     C = get_object_or_404(Comment, pk=comment_id)
     user = get_user(request)
     user_ip = get_client_ip(request)
-    if CommentIpLog.objects.filter(ip=user_ip, comment=C).exists():
-        return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
-    else:
-        if get_user(request).is_active:
-            CommentIpLog.objects.create(ip=user_ip,
-                                        user=user,
-                                        comment=C,
-                                        created_at=timezone.now())
-        else:
-            CommentIpLog.objects.create(ip=user_ip,
-                                        comment=C,
-                                        created_at=timezone.now())
-        if request.method == 'POST':
-            if request.POST['vote'] == 'up':
-                C.upvote += 1
-                C.save()
-                return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
-            if request.POST['vote'] == 'down':
-                C.downvote += 1
-                C.save()
-                return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
-            # up이랑 down 이랑 안되는 케이스도 어떻게 커버하는게 좋을듯
-        elif request.method == 'GET':
-            if request.GET['vote'] == 'up':
-                C.upvote += 1
-                C.save()
-                return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
-            if request.GET['vote'] == 'down':
-                C.downvote += 1
-                C.save()
-                return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
-        else:
+    if user_ip:
+        if CommentIpLog.objects.filter(ip=user_ip, comment=C).exists():
             return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
+        else:
+            if get_user(request).is_active:
+                CommentIpLog.objects.create(ip=user_ip,
+                                            user=user,
+                                            comment=C,
+                                            created_at=timezone.now())
+            else:
+                CommentIpLog.objects.create(ip=user_ip,
+                                            comment=C,
+                                            created_at=timezone.now())
+            if request.method == 'POST':
+                if request.POST['vote'] == 'up':
+                    C.upvote += 1
+                    C.save()
+                    return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
+                if request.POST['vote'] == 'down':
+                    C.downvote += 1
+                    C.save()
+                    return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
+                # up이랑 down 이랑 안되는 케이스도 어떻게 커버하는게 좋을듯
+            elif request.method == 'GET':
+                if request.GET['vote'] == 'up':
+                    C.upvote += 1
+                    C.save()
+                    return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
+                if request.GET['vote'] == 'down':
+                    C.downvote += 1
+                    C.save()
+                    return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
+            else:
+                return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id}))
+    else:
+        return HttpResponseRedirect(reverse('boards:board_detail', kwargs={'article_id': C.article_id,
+                                                                           'error_message': "IP 확인 불가"}))
 
 
 def board_comment_delete(request, comment_id):
@@ -510,23 +514,10 @@ def board_profile(request, user_id):
         return HttpResponseRedirect(reverse('boards:board_index'))
 
 
-# def get_client_ip(request):
-#     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-#     if x_forwarded_for:
-#         ip = x_forwarded_for.split(',')[0]
-#     else:
-#         ip = request.META.get('REMOTE_ADDR')
-#     return ip
-
 def get_client_ip(request):
-    try:
-        ip = request.META.get("HTTP_X_FORWARDED_FOR", None)
-        ip = ip.split(", ")[0]
-        return ip
-    except:
-        try:
-            ip = request.META.get("REMOTE_ADDR", "")
-            return ip
-        except:
-            return None
-        return None
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
--- fabfile.py
+++ fabfile.py
@@ -107,10 +107,10 @@ def deploy():
     _update_static_files()
     _update_database()
     _ufw_allow()
-    _setup_for_ssl
-    _grant_ssl_live
+    # _setup_for_ssl
+    # _grant_ssl_live
     _make_virtualhost()
-    _make_virtualhost_https()
+    # _make_virtualhost_https()
     _grant_apache2()
     # _grant_sqlite3()
     _restart_apache2()
@@ -210,7 +210,7 @@ def _ufw_allow():
     sudo("ufw reload")
 
 
-def _make_virtualhost():
+def _make_virtualhost_forhttps():
     script = """'<VirtualHost *:80>
     ServerName {servername}
     <Directory /home/{username}/{project_name}/{project_name}>
@@ -223,6 +223,39 @@ def _make_virtualhost():
     
     Redirect permanent / https://sayproject.site/
     
+    ErrorLog ${{APACHE_LOG_DIR}}/error.log
+    CustomLog ${{APACHE_LOG_DIR}}/access.log combined
+    </VirtualHost>'""".format(
+        static_root=STATIC_ROOT_NAME,
+        username=env.user,
+        project_name=PROJECT_NAME,
+        static_url=STATIC_URL_NAME,
+        servername=REMOTE_HOST,
+        media_url=MEDIA_ROOT
+    )
+    sudo('echo {} > /etc/apache2/sites-available/{}.conf'.format(script, PROJECT_NAME))
+    sudo('sudo a2ensite {}.conf'.format(PROJECT_NAME))
+
+
+def _make_virtualhost():
+    script = """'<VirtualHost *:80>
+    ServerName {servername}
+    Alias /{static_url} /home/{username}/{project_name}/{static_root}
+    Alias /{media_url} /home/{username}/{project_name}/{media_url}
+    <Directory /home/{username}/{project_name}/{media_url}>
+        Require all granted
+    </Directory>
+    <Directory /home/{username}/{project_name}/{static_root}>
+        Require all granted
+    </Directory>
+    <Directory /home/{username}/{project_name}/{project_name}>
+        <Files wsgi.py>
+            Require all granted
+        </Files>
+    </Directory>
+    WSGIDaemonProcess {project_name} python-home=/home/{username}/.virtualenvs/{project_name}/bin/ python-path=/home/{username}/{project_name}
+    WSGIProcessGroup {project_name}
+    WSGIScriptAlias / /home/{username}/{project_name}/{project_name}/wsgi.py
     ErrorLog ${{APACHE_LOG_DIR}}/error.log
     CustomLog ${{APACHE_LOG_DIR}}/access.log combined
     </VirtualHost>'""".format(
